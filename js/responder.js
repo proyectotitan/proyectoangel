3fe6010f8c4f16631f1a73b2b7e7d70e97209bdd
juanjo y oscar <juanjo_oscar@hotmail.com>
2011-02-05 09:27:59 +0100

    realizando responder de mensajes recibidos

--- js/responder.js
+++ js/responder.js
@@ -2,12 +2,14 @@ function crear_respuesta(id_mensaje, enlace, span)
 {
 		$("#form_res").remove();
 		$("#"+id_mensaje+"").html('<form id="form_res"><textarea></textarea><button class= "btn btn-danger">Enviar</button></form>');
-		$("#"+enlace).remove();
-		$("#"+span).html('<a title="Responder este mensaje" rel="tooltip" onClick="borrar_respuesta("id_1", "e_1", "e_borrar_1")" href="#" class="pull-right" id="responder"><i class="icon-share-alt" ></i></a>');	
+		$("#"+enlace+"").remove();
+		$("#"+span+"").html('<a title="Responder este mensaje" rel="tooltip" onClick="borrar_respuesta('+id_mensaje+', '+enlace+', '+span+')" href="#" class="pull-right" id="'+enlace+'"><i class="icon-share-alt" ></i></a>');	
 }
-function crear_respuesta(id_mensaje, enlace, span)
+function borrar_respuesta(id_mensaje, enlace, span)
 {
+	alert("hola");
 	$("#form_res").remove();
-	$("#"+span).html('<a title="Responder este mensaje" rel="tooltip" onClick="crear_respuesta("id_1", "e_1", "e_borrar_1")" href="#" class="pull-right" id="responder"><i class="icon-share-alt" ></i></a>');
+	$("#"+enlace+"").remove();	
+	
 }
 
