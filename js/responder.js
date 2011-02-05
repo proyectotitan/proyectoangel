3afdd3c9a5b4b7526c0b78538391fbdfe23eed91
juanjo y oscar <juanjo_oscar@hotmail.com>
2011-02-05 09:04:37 +0100

    cambiando funcion de responder, no funciona

--- js/responder.js
+++ js/responder.js
@@ -1,12 +1,13 @@
-function crear_respuesta(id_mensaje)
+function crear_respuesta(id_mensaje, enlace, span)
 {
-	if ($("#form_res").length)
-	{ 
 		$("#form_res").remove();
-	}
-	else
-	{
-		$("#form_res").remove();
-		$("#"+id_mensaje+"").html('<form id="form_res"><textarea></textarea><button class= "btn btn-danger">Enviar</button></form>');	
-	}
+		$("#"+id_mensaje+"").html('<form id="form_res"><textarea></textarea><button class= "btn btn-danger">Enviar</button></form>');
+		$("#"+enlace).remove();
+		$("#"+span).html('<a title="Responder este mensaje" rel="tooltip" onClick="borrar_respuesta("id_1", "e_1", "e_borrar_1")" href="#" class="pull-right" id="responder"><i class="icon-share-alt" ></i></a>');	
 }
+function crear_respuesta(id_mensaje, enlace, span)
+{
+	$("#form_res").remove();
+	$("#"+span).html('<a title="Responder este mensaje" rel="tooltip" onClick="crear_respuesta("id_1", "e_1", "e_borrar_1")" href="#" class="pull-right" id="responder"><i class="icon-share-alt" ></i></a>');
+}
+
