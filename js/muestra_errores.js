8067bea9dcb0c09b7640dde3454dd595462340c9
Oscar Bravo <osquitar.7@hotmail.com>
2012-12-09 17:41:06 +0100

    realizada funcion jquery para mostrar errores en formularios y validado el registro de usuario excepto que las claves coincidan

---
+++ js/muestra_errores.js
@@ -0,0 +1,20 @@
+function muestra_errores(errores)
+{
+	var x;
+	x=$("#errores");
+	x.css("alert alert-error");
+	x.html('<div class="alert alert-error">'+errores+'</div>');
+}
+function imagen_errores(campo, mensaje)
+{
+	var x;
+	x=$("#"+campo+"");
+	x.html('&nbsp; <a title="'+mensaje+'" rel="tooltip" href="#"><img src="../img/iconos/glyphicons_207_remove_2.png" width="14" height="14"></a>');
+}
+
+function imagen_correcto(campo)
+{
+	var x;
+	x=$("#"+campo+"");
+	x.html('&nbsp; <img src="../img/iconos/glyphicons_206_ok_2.png" width="14" height="14">');
+}
