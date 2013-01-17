a82e61723fbd03978097975acfc6dc9bcf871358
carlos_portatil <carloswolf666@gmail.com>
2013-01-17 17:13:18 +0100

    Chat mas grande y en todas las paginas

--- js/abrir_ventana.js
+++ js/abrir_ventana.js
@@ -1,4 +1,4 @@
 function Abrir_ventana (pagina) {
-var opciones="toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=yes, width=508, height=365, top=85, left=140";
+var opciones="toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=yes, width=718, height=465, top=85, left=140";
 window.open(pagina,"",opciones);
 }
