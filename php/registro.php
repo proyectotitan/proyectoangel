8c06ff6fbcdeb83158de6a456d2c60365ede7fb7
juanjo <juanjo@gmail.com>
2013-02-14 17:20:16 +0100

    encontrada forma para sacar codigo error y error excepciones mysql

--- php/registro.php
+++ php/registro.php
@@ -3,12 +3,19 @@
 $conexion = mysql_connect ("localhost","proyecto","proyecto");
 
 mysql_select_db("proyecto",$conexion);
-try{
-mysql_query("INSERT INTO usuario (nombre,pass,correo) VALUES ('{$_POST['r_usuario']}','{$_POST['r_usuario']}','{$_POST['r_usuario']}')");
 
-}catch (Exception $e) {
-    $error = $e->getMessage();
+$insertado=mysql_query("INSERT INTO usuario (nombre,pass,correo) VALUES ('{$_POST['r_usuario']}','{$_POST['r_usuario']}','{$_POST['r_usuario']}')");
+
+if(!$insertado){
+$x=mysql_errno();
 }
+
+if($x==1062){
+die("cuack!");
+}
+//mysql_error muestra el error producido
+
 mysql_close($conexion);
+
 header('Location: index.php');
 ?>
