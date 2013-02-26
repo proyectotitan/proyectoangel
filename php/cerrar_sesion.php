1d1d723a3ed2e82a5f31649620212c4a70c15da0
juanjo <juanjo@gmail.com>
2013-02-26 18:34:53 +0100

    terminado inicio y cierre de sesion

--- php/cerrar_sesion.php
+++ php/cerrar_sesion.php
@@ -1,5 +1,9 @@
 <?php
 	session_start();
+	$usuario = $_SESSION["usuario"];
+	$conexion = mysql_connect ("localhost","proyecto","proyecto");
+	mysql_select_db("proyecto",$conexion);
+    mysql_query ("UPDATE usuario SET sesion = '0' WHERE nombre = '$usuario'");
 	session_destroy();
 	$_SESSION["usuario"]="";
 
