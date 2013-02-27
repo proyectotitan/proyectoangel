5883018c3334abd01898af60e220eefab79a51e4
carlos_portatil <carloswolf666@gmail.com>
2013-02-27 19:37:10 +0100

    Optimicado enviar_mail pero sin funcionar

--- php/enviar_mail.php
+++ php/enviar_mail.php
@@ -1,5 +1,10 @@
 <?php
-
-mail('carloswolf666@gmail.com','Comprobación del Mercury','Si tu lees ésto es que he enviado un email con Mercury desde un PHP YEAH!!!');
-
+	if(mail('carloswolf666@gmail.com','Comprobación del Mercury','Si tu lees ésto es que he enviado un email con Mercury desde un PHP YEAH!!!'))
+	{
+		echo "Email enviado.";
+	}
+	else
+	{
+		echo "Error.!!";	
+	}
 ?>
